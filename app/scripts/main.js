493e6a6411fc7559387892f959da6281a1a6c9d1
Poltergeist <spectator.networx@gmail.com>
2014-08-18 12:29:36 +0200

    fix table select.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,19 @@
 var $table = document.querySelector('#priority'),
   result = {
   },
-  $result = document.querySelector('.result');
+  $result = document.querySelector('.result'),
+  clearRow = function(element){
+    var $selected = element.parentElement.querySelector('.selected'),
+      oldClass = $selected && $selected.classList.item(0),
+      active = $table.querySelectorAll('.' + oldClass);
+
+    $selected && $selected.classList.remove('selected');
+    if(active) {
+      [].forEach.call(active, function(item) {
+        item.classList.remove('notSelect');
+      });
+    }
+  };
 $table.addEventListener('click', function(event){
   var $selected = event.srcElement.parentElement.querySelector('.selected'),
     classSelected = event.srcElement.classList.item(0),
@@ -9,6 +21,10 @@ $table.addEventListener('click', function(event){
     active = document.querySelectorAll('.' + oldClass),
     deactive = document.querySelectorAll('.' + classSelected),
     type;
+  if(event.srcElement.classList.contains('remove')){
+    clearRow(event.srcElement);
+    return;
+  }
   if(event.srcElement.classList.contains('notSelect') || event.srcElement.tagName.toLowerCase() != 'td'){
     return;
   }
@@ -16,20 +32,21 @@ $table.addEventListener('click', function(event){
   $selected && $selected.classList.remove('selected');
 
   if(active) {
-    console.log(active);
     [].forEach.call(active, function(item) {
       item.classList.remove('notSelect');
     });
   }
   [].forEach.call(deactive, function(item) {
-    if (item == event.srcElement) { console.log(item); return;}
+    if (item == event.srcElement) { return;}
     item.classList.add('notSelect');
   });
   event.srcElement.classList.add('selected');
   type = event.srcElement.parentElement.getAttribute('data-type');
   result[type] = event.srcElement.innerText;
-  console.log(result);
 
-  $result.innerHTML = result.race + result.income + result.type + result.skills + result.attribute;
+  $result.innerHTML = '';
+  Object.keys(result).forEach(function(item) {
+    $result.innerHTML += result[item];
+  });
 
 });
